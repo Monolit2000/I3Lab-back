005df1c9caeb0797be8f4197889e32a7e4e4039d
Monolit2000 <kostuchenkojr@gmail.com>
2024-07-04 20:59:30 +0300

    MemberConfiguration bug fix

--- src/Modules/Work/I3Lab.Work.Application/Members/CreateMember/CreateMemberCommandHandler.cs
+++ src/Modules/Work/I3Lab.Work.Application/Members/CreateMember/CreateMemberCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentResults;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace I3Lab.Works.Application.Members.CreateMember
 {
-    internal class CreateMemberCommandHandler
+    public class CreateMemberCommandHandler : IRequest<Result<MemberDto>>
     {
     }
 }
