15cb5184a3e47d778c52329a11cab549a86a0531
Monolit2000 <46726840+Monolit2000@users.noreply.github.com>
2024-07-11 23:48:21 +0300

    GetBlobQurey added

---
+++ src/Modules/Work/I3Lab.Work.Application/BlobFiles/GetBlobFile/BlobFileDto.cs
@@ -0,0 +1,21 @@
+﻿using I3Lab.Works.Domain.BlobFiles;
+using I3Lab.Works.Domain.WorkDirectorys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I3Lab.Works.Application.BlobFiles.GetBlobFile
+{
+    public class BlobFileDto
+    {
+        public Stream Stream { get; set; }
+
+        public BlobFileDto(
+            Stream stream)
+        {
+            Stream = stream;
+        }
+    }
+}
---
+++ src/Modules/Work/I3Lab.Work.Application/BlobFiles/GetBlobFile/GetBlobFileQuerie.cs
@@ -0,0 +1,21 @@
+﻿using FluentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I3Lab.Works.Application.BlobFiles.GetBlobFile
+{
+    public class GetBlobFileQuerie : IRequest<Result<BlobFileDto>>
+    {
+        public Guid BlobFileId { get; set; }
+
+        public GetBlobFileQuerie(Guid blobFileId)
+        {
+            BlobFileId = blobFileId;
+        }
+
+    }
+}
