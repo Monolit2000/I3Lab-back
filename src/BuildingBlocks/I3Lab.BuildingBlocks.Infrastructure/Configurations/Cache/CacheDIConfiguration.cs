d557a86180fb20c7bf3d866b03734b87fede7e24
Андрей Костюченко <kostuchenkojr@gmail.com>
2024-08-19 14:36:55 +0300

    Added cache aside extensions

---
+++ src/BuildingBlocks/I3Lab.BuildingBlocks.Infrastructure/Configurations/Cache/CacheDIConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using I3Lab.Works.Application.BlobFiles.AddBlobFile;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I3Lab.BuildingBlocks.Infrastructure.Configurations.Cache
+{
+    public static class CacheDIConfiguration
+    {
+        public static IServiceCollection AddCacheAsideService(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDistributedMemoryCache();
+
+            return services;
+        }
+    }
+}
--- src/BuildingBlocks/I3Lab.BuildingBlocks.Infrastructure/StartUp/BuildingBlocksStartUp.cs
+++ src/BuildingBlocks/I3Lab.BuildingBlocks.Infrastructure/StartUp/BuildingBlocksStartUp.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using I3Lab.BuildingBlocks.Infrastructure.ExecutionContext;
 using I3Lab.BuildingBlocks.Infrastructure.Configurations.BlobStorage;
+using I3Lab.BuildingBlocks.Infrastructure.Configurations.Cache;
 
 namespace I3Lab.BuildingBlocks.Infrastructure.StartUp
 {
@@ -16,6 +17,8 @@ namespace I3Lab.BuildingBlocks.Infrastructure.StartUp
             services.AddBlobStorage(configuration);
 
             services.AddHealthChecks();
+
+            services.AddCacheAsideService(configuration);
 
             return services;
         }
