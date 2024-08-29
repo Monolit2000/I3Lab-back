2c4e5c26da2f3176bf2db7f589cfd663b5d1c457
Андрей Костюченко <kostuchenkojr@gmail.com>
2024-08-29 13:25:31 +0300

    udpate

--- src/Modules/Doctor/I3Lab.Doctor.Application/DoctorCreationProposals/ConfirmDoctorCreationProposal/ConfirmDoctorCreationProposalCommandHandler.cs
+++ src/Modules/Doctor/I3Lab.Doctor.Application/DoctorCreationProposals/ConfirmDoctorCreationProposal/ConfirmDoctorCreationProposalCommandHandler.cs
@@ -1,11 +1,7 @@
 ﻿using FluentResults;
 using I3Lab.Doctors.Domain.DoctorCreationProposals;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace I3Lab.Doctors.Application.DoctorCreationProposals.ConfirmDoctorCreationProposal
 {
---
+++ src/Modules/Doctor/I3Lab.Doctor.Application/DoctorCreationProposals/ConfirmDoctorCreationProposal/DoctorCreationProposalConfirmedDomainEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using I3Lab.Doctors.Domain.DoctorCreationProposals;
+using I3Lab.Doctors.Domain.DoctorCreationProposals.Events;
+using MediatR;
+
+namespace I3Lab.Doctors.Application.DoctorCreationProposals.ConfirmDoctorCreationProposal
+{
+    public class DoctorCreationProposalConfirmedDomainEventHandler(
+        ISender sender) : INotificationHandler<DoctorCreationProposalConfirmedDomainEvent>
+    {
+        public Task Handle(DoctorCreationProposalConfirmedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
