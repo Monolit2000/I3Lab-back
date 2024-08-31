c3cf34eb6096fd506dc4d3ae5f89a44ea3690156
Андрей Костюченко <kostuchenkojr@gmail.com>
2024-08-31 18:39:39 +0300

    update

--- src/Modules/Administration/I3Lab.Administration.Infrastructure/Configurations/Persistence/PersistenceDIConfiguration.cs
+++ src/Modules/Administration/I3Lab.Administration.Infrastructure/Configurations/Persistence/PersistenceDIConfiguration.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using I3Lab.Administration.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using I3Lab.BuildingBlocks.Infrastructure;
+using I3Lab.BuildingBlocks.Infrastructure.Domain;
+
 
 namespace I3Lab.Administration.Infrastructure.Configurations.Persistence
 {
-    public class PersistenceDIConfiguration
+    public static class PersistenceDIConfiguration
     {
+        public static IServiceCollection AddPersistenceServices(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<AdministrationContext>((sp, options) =>
+            {
+                options.ReplaceService<IValueConverterSelector, StronglyTypedIdValueConverterSelector>();
+                options.UseNpgsql(configuration.GetConnectionString("Database"));
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            });
+
+            return services;
+        }
     }
 }
--- src/Modules/Administration/I3Lab.Administration.Infrastructure/Persistence/Extensions/MigrationExtensions.cs
+++ src/Modules/Administration/I3Lab.Administration.Infrastructure/Persistence/Extensions/MigrationExtensions.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace I3Lab.Administration.Infrastructure.Persistence.Extensions
 {
-    internal class MigrationExtensions
+    public static class MigrationExtensions
     {
+        public static void ApplyDoctorContextMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+            using AdministrationContext administrationContext = scope.ServiceProvider.GetRequiredService<AdministrationContext>();
+
+            //workContext.Database.EnsureDeleted();
+
+            administrationContext.Database.Migrate();
+        }
     }
 }
